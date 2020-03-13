2e87aa357788a26fa2e12f5509af32afeb9f9f72
Wang Zijun <wzj97@hotmail.com>
2020-03-14 03:17:14 +0800

    Updates

---
+++ precache-manifest.b7404f8635b4da2a45476a79756af7bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4abbaf0e820e99149404ace63d4b59e",
+    "url": "/the-plan/index.html"
+  },
+  {
+    "revision": "1471b438312bdd19bd38",
+    "url": "/the-plan/static/css/main.d35dd769.chunk.css"
+  },
+  {
+    "revision": "0891042794d3af19a3c0",
+    "url": "/the-plan/static/js/2.e1dd36c4.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/the-plan/static/js/2.e1dd36c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1471b438312bdd19bd38",
+    "url": "/the-plan/static/js/main.438f0e37.chunk.js"
+  },
+  {
+    "revision": "47f4e04592f60962c12a",
+    "url": "/the-plan/static/js/runtime-main.f5f3f57c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/the-plan/static/media/logo.5d5d9eef.svg"
+  }
+]);
